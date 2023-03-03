663b5c5aa609563f1a7cb32756c2ea19ec33d95c
LT <893449325@qq.com>
2023-03-03 23:55:15 +0800

    属性预测更新

---
+++ Abstract_predict_Demo_v2/code/in_out/dataloader_to_pack.py
@@ -0,0 +1,70 @@
+
+import torch
+import os.path as osp
+import multiprocessing as mp
+import torchvision.transforms as transforms
+
+from PIL import Image
+
+from  Abstract_predict_Demo_v2.code.data_object.WordsPredictDataset import WordsPredictDataset
+
+image_net_mean = [0.485, 0.456, 0.406]
+image_net_std = [0.229, 0.224, 0.225]
+
+def max_io_workers():
+    """return all/max possible available cpus of machine."""
+    return max(mp.cpu_count() - 1, 1)
+
+def image_transformation(img_dim, lanczos=True):
+    """simple transformation/pre-processing of image data."""
+
+    if lanczos:
+        resample_method = Image.LANCZOS
+    else:
+        resample_method = Image.BILINEAR
+
+    normalize = transforms.Normalize(mean=image_net_mean, std=image_net_std)
+    img_transforms = dict()
+    img_transforms['train'] = transforms.Compose([transforms.Resize((img_dim, img_dim), resample_method),
+                                                  transforms.ToTensor(),
+                                                  normalize])
+
+    # Use same transformations as in train (since no data-augmentation is applied in train)
+    img_transforms['test'] = img_transforms['train']
+    img_transforms['val'] = img_transforms['train']
+    img_transforms['rest'] = img_transforms['train']
+    return img_transforms
+
+
+def dataloader_pack(df, args, drop_thres=None):
+    dataloaders = dict()
+    datasets = dict()
+    img_transforms = image_transformation(args.img_dim, lanczos=args.lanczos)
+
+    if args.num_workers == -1:
+        n_workers = max_io_workers()
+    else:
+        n_workers = args.num_workers
+
+    for split, g in df.groupby('split'):
+        g.reset_index(inplace=True, drop=True)
+
+        if split == 'train' and drop_thres is not None:
+            noise_mask = g['emotion_distribution'].apply(lambda x: max(x) > drop_thres)
+            print('Keeping {} of the training data, since for the rest their emotion-maximizer is too low.'.format(noise_mask.mean()))
+            g = g[noise_mask]
+            g.reset_index(inplace=True, drop=True)
+
+
+        img_files = g.apply(lambda x : osp.join(args.img_dir, x.art_style,  x.painting + '.jpg'), axis=1)
+        img_files.name = 'image_files'
+        dataset = WordsPredictDataset(img_files, g.abs_feature,
+                                             img_transform=img_transforms[split])
+
+        datasets[split] = dataset
+        b_size = args.batch_size if split=='train' else args.batch_size * 2
+        dataloaders[split] = torch.utils.data.DataLoader(dataset=dataset,
+                                                         batch_size=b_size,
+                                                         shuffle=split=='train',
+                                                         num_workers=n_workers)
+    return dataloaders, datasets
---
+++ Abstract_predict_Demo_v2/code/original_model/neural_models/image_emotion_clf.py
@@ -0,0 +1,76 @@
+"""
+Given an image guess a distribution over the emotion labels.
+
+The MIT License (MIT)
+Originally created in 2020, for Python 3.x
+Copyright (c) 2021 Panos Achlioptas (ai.stanford.edu/~optas) & Stanford Geometric Computing Lab
+"""
+
+import torch
+import torch.nn.functional as F
+from torch import nn
+import tqdm
+
+from artemis.utils.stats import AverageMeter
+
+
+class ImageWordsClassifier(nn.Module):
+    def __init__(self, img_encoder, clf_head):
+        super(ImageWordsClassifier, self).__init__()
+        self.img_encoder = img_encoder
+        self.clf_head = clf_head
+
+    def __call__(self, img,abstract_data):
+        feat = self.img_encoder(img)
+        logits = self.clf_head(feat)
+        return logits
+
+
+def single_epoch_train(model, data_loader, criterion, optimizer, device,args=None):
+    epoch_loss = AverageMeter()
+    model.train()
+    for batch in tqdm.tqdm(data_loader):
+        img = batch['image'].to(device)
+        abstract_data = batch['abstract_data'].to(device)
+        logits = model(img,abstract_data)
+
+        # Calculate loss
+        loss = criterion(logits, abstract_data)
+
+        # Back prop.
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        b_size = len(abstract_data)
+        epoch_loss.update(loss.item(), b_size)
+    return epoch_loss.avg
+
+
+@torch.no_grad()
+def evaluate_on_dataset(model, data_loader, criterion, device, detailed=True, kl_div=True,args=None):
+    epoch_loss = AverageMeter()
+    model.eval()
+    epoch_confidence = []
+    for batch in tqdm.tqdm(data_loader):
+        img = batch['image'].to(device)
+        abstract_data = batch['abstract_data'].to(device)
+        logits = model(img,abstract_data)
+
+        # Calculate loss
+        loss = criterion(logits, abstract_data)
+        detailed=True
+        kl_div=False
+        if detailed:
+            if kl_div:
+                epoch_confidence.append(torch.exp(logits).cpu())  # logits are log-soft-max
+            else:
+                epoch_confidence.append(F.softmax(logits, dim=-1).cpu()) # logits are pure logits
+
+        b_size = len(abstract_data)
+        epoch_loss.update(loss.item(), b_size)
+
+    if detailed:
+        epoch_confidence = torch.cat(epoch_confidence).numpy()
+
+    return epoch_loss.avg, epoch_confidence
